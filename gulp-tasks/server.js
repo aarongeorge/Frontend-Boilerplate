38be521f2dd314746f7994ebf2c80d194bf4001d
Aaron George <aaron.dean.george@gmail.com>
2017-08-29 11:54:49 +1000

    UPDATED: ESLint rules
    ADDED: BrowserHistory API support for the dev server

--- gulp-tasks/server.js
+++ gulp-tasks/server.js
@@ -2,7 +2,11 @@
  * Tasks: Server
  *
  * Runs a server, serving up the contents in paths.webroot.root on port 1337
+ * It also supports the History API for SPA's
  */
+
+// Dependencies
+const historyApiFallback = require('connect-history-api-fallback');
 
 // Task
 module.exports = (gulp, paths, browserSync) => {
@@ -10,7 +14,8 @@ module.exports = (gulp, paths, browserSync) => {
         browserSync.init({
             'server': {
                 'baseDir': paths.webroot.root,
-                'directory': false
+                'directory': false,
+                'middleware': [historyApiFallback()]
             },
             'ghostMode': false,
             'port': 1337,
