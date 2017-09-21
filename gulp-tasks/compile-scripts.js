896c1ecb3c77f6fb4b02dbf37de088936d49169f
Aaron George <aaron.dean.george@gmail.com>
2017-09-21 11:16:37 +1000

    ADDED: Babelrc
    UPDATED: Compile scripts to allow multiple bundles ot be created

--- gulp-tasks/compile-scripts.js
+++ gulp-tasks/compile-scripts.js
@@ -11,30 +11,47 @@ const babelify = require('babelify');
 const browserify = require('browserify');
 const buffer = require('vinyl-buffer');
 const gulpif = require('gulp-if');
+const merge = require('merge-stream');
+const rename = require('gulp-rename');
 const source = require('vinyl-source-stream');
 const sourcemaps = require('gulp-sourcemaps');
 const uglify = require('gulp-uglify');
 
-// Task
 module.exports = (gulp, paths) => {
+
     return () => {
-        return browserify(
+
+        const entries = [
             {
-                'entries': [`${paths.src.scripts}main.js`],
-                'debug': true
-            })
-            .transform(babelify, {
-                'presets': [
-                    'es2015',
-                    'stage-2'
-                ]
-            })
-            .bundle()
-            .pipe(source('main.js'))
-            .pipe(buffer())
-            .pipe(gulpif(gulp.environment !== 'prod', sourcemaps.init({'loadMaps': true})))
-            .pipe(gulpif(gulp.environment === 'prod', uglify()))
-            .pipe(gulpif(gulp.environment !== 'prod', sourcemaps.write('./')))
-            .pipe(gulp.dest(paths.webroot.scripts));
+                'filename': 'main.js',
+                'basePath': `${paths.src.scripts}`,
+                'outputPath': `${paths.webroot.scripts}`,
+                'rename': 'main'
+            }
+        ];
+
+        return merge(entries.map((entry) => {
+            return browserify(
+                {
+                    'entries': entry.basePath + entry.filename,
+                    'debug': true
+                })
+                .transform(babelify, {
+                    'presets': [
+                        'env',
+                        'stage-2'
+                    ]
+                })
+                .bundle()
+                .pipe(source(entry.filename))
+                .pipe(rename({
+                    'basename': entry.rename
+                }))
+                .pipe(buffer())
+                .pipe(gulpif(gulp.environment !== 'prod', sourcemaps.init({'loadMaps': true})))
+                .pipe(gulpif(gulp.environment === 'prod', uglify()))
+                .pipe(gulpif(gulp.environment !== 'prod', sourcemaps.write('./')))
+                .pipe(gulp.dest(entry.outputPath));
+        }));
     };
 };
